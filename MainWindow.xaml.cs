0de2d4f45b05f60d6f8f4d045dde723a644d7949
gentleman-8 <roc.no2004@gmail.com>
2023-03-27 13:07:27 +0800

    3全部都能轉換
    3全部都能轉換

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -33,10 +33,19 @@ namespace _20230327
         private void txtCM_KeyUp(object sender, KeyEventArgs e)
         {
             double douCM; //宣告一個double變數，變數名稱叫douCM
-
             douCM = Convert.ToDouble(txtCM.Text); //從txtCM輸入文字框取得輸入的文字，並且轉換成double的資料型態
 
+            //透過string.Format格式化成小數點後共10位的數字，轉型成文字型態，在txtM顯示結果
             txtM.Text = string.Format("{0:0.##########}", douCM / 100);
+            txtKM.Text = string.Format("{0:0.##########}", douCM / 100000);
+            txtIn.Text = string.Format("{0:0.##########}", douCM / 2.54);
+            txtFt.Text = string.Format("{0:0.##########}", douCM / 30.48);
+            txtYard.Text = string.Format("{0:0.##########}", douCM / 91.44);
+        }
+
+        private void txtCM_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
